2e18b97bc750dfb25b37c337622cae3c5891cbb1
Ian <ianhitchcock@outlook.com>
2017-10-20 18:04:21 -0700

    successfully filters and gets info from api, adds layout for mobile and tablet

--- js/index.js
+++ js/index.js
@@ -8,16 +8,41 @@ $(function(){
     $('.dynamic-header__logo-img')
       .addClass('dynamic-header--banner__logo-img')
       .removeClass('dynamic-header--cover__logo-img');
-    $('.footer').css('margin', '0 0 4.8rem');
+    $('.footer').css('margin', '4.8rem 0 4.8rem');
 
-    //inject loading
-    $('.main').append('<img class="main__loading-img" src="images/ajax-loader.gif">');
+    //empty main, inject loading gif
+
+    $('.main').empty().append('<img class="main__loading-img" src="images/ajax-loader.gif">');
   
-
+    //fetch content--remove loading-gif--inject stories
+    $.getJSON('http://api.nytimes.com/svc/topstories/v2/' + $('#choose-section').val() +'.json?api-key=b038a37bb5974147b7e6bf0e97916430', function(data){
+      $('.main__loading-img').remove();
+      console.log(data);
+      //check to make sure image over 400px exists
+      var results = data.results.filter(function(result){
+        for (var i =0; i < result.multimedia.length; i++) {
+          if (result.multimedia[i].height >= 400) {
+            return true;
+          }
+        }
+      });
+      $.each(results , function( i , val ){
+        var count = (i+1);
+        var article = 'main__article--' + (i+1);
+        var caption = val.abstract;
+        var url = val.multimedia[val.multimedia.length - 1].url;
+        console.log(url);
+        $('.main').append('<article class="main__article ' + article + '"><h2 class="main__caption">' + caption + '</h2></article>');
+        $('.' + article).css('background-image' , 'url(' + url + ')');
+        if (count === 12) {
+          return false;
+        }
+      });
+    });
 
   });
   
-})
+});
 
 
 
