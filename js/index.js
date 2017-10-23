32276e395deffc499533c6ca4f8ea2bd4b5e8b62
Ian <ianhitchcock@outlook.com>
2017-10-23 14:40:46 -0700

    Add .fail() function and add comments to index.js

--- js/index.js
+++ js/index.js
@@ -11,11 +11,13 @@ $(function(){
     $('.footer').css('margin', '4.8rem 0 4.8rem');
 
     //empty main, inject loading gif
-
     $('.main').empty().append('<img class="main__loading-img" src="images/ajax-loader.gif">');
   
     //fetch content--remove loading-gif--inject stories
-    $.getJSON('http://api.nytimes.com/svc/topstories/v2/' + $('#choose-section').val() +'.json?api-key=b038a37bb5974147b7e6bf0e97916430', function(data){
+    $.getJSON('http://api.nyts.com/svc/topstories/v2/' + $('#choose-section').val() +'.json?api-key=b038a37bb5974147b7e6bf0e97916430', function(){
+
+    }).done(function(data){
+
       //check to make sure last image over 400px exists
       var results = data.results.filter(function(result){
         if (result.multimedia.length){
@@ -25,6 +27,7 @@ $(function(){
         }
       });
       $('.main__loading-img').remove();
+
       $.each(results , function( i , val ){
         var count = (i+1);
         var article = 'main__article--' + (i+1);
@@ -33,23 +36,26 @@ $(function(){
         var imageUrl = val.multimedia[val.multimedia.length - 1].url;
         $('.main').append('<a target="_blank" href="' + nytUrl + '"><article class="main__article main__article--loading ' + article + '"><p class="main__caption">' + caption + '</p></article>');
         $('.' + article).css('background-image' , 'url(images/ajax-loader.gif)');
+
+        //to wait for bg image to load before applying style
         var img = new Image();
         img.onload = function(){
           $('.' + article).removeClass('main__article--loading').css('background-image' , 'url(' + imageUrl + ')');
         };
         img.src = imageUrl;
+
+        //break .each loop after 12 iterations
         if (count === 12) {
         return false;
         }
       });
-    }).done(function(){
-
     }).fail(function(){
-      $('.main__loading-img').empty().append('<p> Request failed. Please try again </p>');
+      //empty .main inject error message
+      $('.main').empty().append('<p> Request failed. Please try again </p>');
     });
+    //did not include always to remove loading gif as it was just as easy to apply .empty() to the chain in .done and .fail
 
   });
-  
 });
 
 
